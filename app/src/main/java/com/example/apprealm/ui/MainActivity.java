35223bb05df4b3f7f03b69818db72173a8e99243
Yamil Vogl <yamil.vogl@regxi.com>
2021-08-05 16:28:05 -0600

    validaciones e insertar datos

--- app/src/main/java/com/example/apprealm/ui/MainActivity.java
+++ app/src/main/java/com/example/apprealm/ui/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.apprealm.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.apprealm.R;
 import com.example.apprealm.crud.CrudProfesor;
 import com.example.apprealm.model.Profesor;
@@ -24,14 +29,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         realm = Realm.getDefaultInstance();
-        configView();
-    }
-    public void configView(){
-        profesor = new  Profesor();
+        profesor = new Profesor();
         etEmail = findViewById(R.id.etEmail);
         etNombre = findViewById(R.id.etName);
         btGuardar = findViewById(R.id.btGuardar);
 
+        configView();
+    }
+
+    public void configView() {
         //Guardamos campos
         btGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 profesor.setName(etNombre.getText().toString());
                 profesor.setEmail(etEmail.getText().toString());
                 CrudProfesor.addProfesor(profesor);
+                clear();
+                etNombre.requestFocus();
             }
         });
         btMostrar = findViewById(R.id.btMostrar);
@@ -48,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
                 CrudProfesor.getAllProfesor();
             }
         });
+
+        etNombre.addTextChangedListener(buttonWatcher);
+        etEmail.addTextChangedListener(buttonWatcher);
     }
 
+    public void clear() {
+        etNombre.setText("");
+        etEmail.setText("");
+
+    }
+
+    private TextWatcher buttonWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String nameInput = etNombre.getText().toString().trim();
+            String emailInput = etEmail.getText().toString().trim();
+
+            btGuardar.setEnabled(!nameInput.isEmpty() && !emailInput.isEmpty());
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
